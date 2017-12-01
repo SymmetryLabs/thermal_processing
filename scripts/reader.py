618bf9f4a931b66d4cb77394ccb49f408f04e5dc
Raphie Palefsky-Smith <me@raphieps.com>
2017-11-30 19:34:35 -0800

    Update reader.py to show output

--- scripts/reader.py
+++ scripts/reader.py
@@ -6,14 +6,18 @@ import time
 import sys
 from processor import FrameProcessor, Timer
 
+# usage: python reader.py glob/to/frame/files*.pgm
+
 def main():
     fp = FrameProcessor()
     files = sorted(sys.argv[1:])
 
     for f in files[:-10]:
         im = cv2.imread(f, cv2.CV_8UC1)
-        # big = cv2.resize(im, (0, 0), fx=2, fy=2)
-        fp.process(im)
+        out = fp.process(im)
+        if out is not None:
+	        cv2.imshow("out", out)
+	        cv2.waitKey(1)
 
    
 
